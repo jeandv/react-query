3039eaf21f03eba6079e649a2dc3e40d3608d7a7
Jean Rondon <jeanrdev@gmail.com>
2022-11-28 22:51:01 -0400

    getting labels from the api and using useQuery

--- 02-react-query-issues-main/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues-main/src/issues/components/LabelPicker.tsx
@@ -1,8 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+
+const API_LABELS = 'https://api.github.com/repos/facebook/react/labels'
+
+const getLabels = async () => {
+
+  const res = await fetch(API_LABELS);
+
+  const data = await res.json();
+  console.log(data)
+  return data;
+
+}
+
 export const LabelPicker = () => {
+
+  const labelsQuery = useQuery(
+    ['labels'],
+    getLabels
+  );
+
   return (
     <div>
       <span
-        className="badge rounded-pill m-1 label-picker"
+        className='badge rounded-pill m-1 label-picker'
         style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
       >
         Primary

e0121ddc0e19cfb80b3fa505c1b8e3657e51af60
Jean Rondon <jeanrdev@gmail.com>
2023-01-21 02:27:37 -0400

    changing github token to make api request work

--- 02-react-query-issues-main/src/api/githubApi.ts
+++ 02-react-query-issues-main/src/api/githubApi.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization: 'Bearer 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'
+    Authorization: 'Bearer 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'
   }
 });
